a8255b2745d302cfc2ae5e6de9a81027db5b87e5
phamhao <phamhao>
2025-03-22 15:23:06 +0700

    RSA and ECC

--- lab-02/api.py
+++ lab-02/api.py
@@ -24,9 +24,9 @@ def caesar_encrypt():
     plain_text=data['plain_text']
     key=int(data['key'])
     encrypted_text=caesar_cipher.encrypt_text(plain_text,key)
-    return jsonify({'encrupted_message':encrypted_text})
+    return jsonify({'encrypted_message':encrypted_text})
 
-@app.route("/api/caesar/decrypt",methods=["POST"])
+@app.route("/api/ceasar/decrypt",methods=["POST"])
 def caesar_decrypt():
     data=request.json
     cipher_text=data['cipher_text']
